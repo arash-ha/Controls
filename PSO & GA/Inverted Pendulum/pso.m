38cc2a816f034dfffdcb0e59e8f905f29d4e08b9
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2020-10-24 19:48:51 -0400

    Create pso.m

---
+++ PSO & GA/Inverted Pendulum/pso.m
@@ -0,0 +1,151 @@
+clc;
+clear;
+close all;
+
+%% Problem Definition
+
+params = GetParams();
+
+CostFunction = @(K) PerformanceIndex(K,params);     % Cost Function
+
+VarSize = [params.nu params.nx];   % Decision Variables Matrix Size
+
+nVar = prod(VarSize); % Number of Decision Variables
+
+VarMin = -1;         % Lower Bound of Variables
+VarMax = 1;         % Upper Bound of Variables
+
+
+%% PSO Parameters
+
+MaxIt = 100;      % Maximum Number of Iterations
+
+nPop = 25;        % Population Size (Swarm Size)
+
+w=1;            % Inertia Weight
+wdamp=0.99;     % Inertia Weight Damping Ratio
+c1=1.5;         % Personal Learning Coefficient
+c2=2;           % Global Learning Coefficient
+
+% Constriction Coefficients
+% phi1=2.05;
+% phi2=2.05;
+% phi=phi1+phi2;
+% chi=2/(phi-2+sqrt(phi^2-4*phi));
+% w=chi;          % Inertia Weight
+% wdamp=1;        % Inertia Weight Damping Ratio
+% c1=chi*phi1;    % Personal Learning Coefficient
+% c2=chi*phi2;    % Global Learning Coefficient
+
+% Velocity Limits
+VelMax=0.1*(VarMax-VarMin);
+VelMin=-VelMax;
+
+%% Initialization
+
+empty_particle.Position=[];
+empty_particle.Cost=[];
+empty_particle.Out=[];
+empty_particle.Velocity=[];
+empty_particle.Best.Position=[];
+empty_particle.Best.Cost=[];
+empty_particle.Best.Out=[];
+
+particle=repmat(empty_particle,nPop,1);
+
+BestSol.Cost=inf;
+
+for i=1:nPop
+    
+    % Initialize Position
+    particle(i).Position=unifrnd(VarMin,VarMax,VarSize);
+    
+    % Initialize Velocity
+    particle(i).Velocity=zeros(VarSize);
+    
+    % Evaluation
+    [particle(i).Cost, particle(i).Out]=CostFunction(particle(i).Position);
+    
+    % Update Personal Best
+    particle(i).Best.Position=particle(i).Position;
+    particle(i).Best.Cost=particle(i).Cost;
+    particle(i).Best.Out=particle(i).Out;
+    
+    % Update Global Best
+    if particle(i).Best.Cost<BestSol.Cost
+        
+        BestSol=particle(i).Best;
+        
+    end
+    
+end
+
+BestCost=zeros(MaxIt,1);
+
+
+%% PSO Main Loop
+
+for it=1:MaxIt
+    
+    for i=1:nPop
+        
+        % Update Velocity
+        particle(i).Velocity = w*particle(i).Velocity ...
+            +c1*rand(VarSize).*(particle(i).Best.Position-particle(i).Position) ...
+            +c2*rand(VarSize).*(BestSol.Position-particle(i).Position);
+        
+        % Apply Velocity Limits
+        particle(i).Velocity = max(particle(i).Velocity,VelMin);
+        particle(i).Velocity = min(particle(i).Velocity,VelMax);
+        
+        % Update Position
+        particle(i).Position = particle(i).Position + particle(i).Velocity;
+        
+        % Velocity Mirror Effect
+        IsOutside=(particle(i).Position<VarMin | particle(i).Position>VarMax);
+        particle(i).Velocity(IsOutside)=-particle(i).Velocity(IsOutside);
+        
+        % Apply Position Limits
+        particle(i).Position = max(particle(i).Position,VarMin);
+        particle(i).Position = min(particle(i).Position,VarMax);
+        
+        % Evaluation
+        [particle(i).Cost, particle(i).Out] = CostFunction(particle(i).Position);
+        
+        % Update Personal Best
+        if particle(i).Cost<particle(i).Best.Cost
+            
+            particle(i).Best.Position=particle(i).Position;
+            particle(i).Best.Cost=particle(i).Cost;
+            particle(i).Best.Out=particle(i).Out;
+            
+            % Update Global Best
+            if particle(i).Best.Cost<BestSol.Cost
+                
+                BestSol=particle(i).Best;
+                
+            end
+            
+        end
+        
+    end
+    
+    w=w*wdamp;
+    
+    BestCost(it)=BestSol.Cost;
+    
+    disp(['Iteration ' num2str(it) ': Best Cost = ' num2str(BestCost(it))]);
+
+    % Plot Solution
+    figure(1);
+    PlotSolution(BestSol);
+    pause(0.01);
+
+end
+
+%% Results
+
+figure;
+plot(BestCost,'LineWidth',2);
+xlabel('Iteration');
+ylabel('Best Cost');
